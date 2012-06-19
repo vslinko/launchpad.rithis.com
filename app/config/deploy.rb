04278a0c15756309cf80c4cf3acb5b86be15e32e
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-06-19 17:12:53 +0400

    Keep 5 releases is default value

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -9,7 +9,6 @@ set :shared_files, ["app/config/parameters.yml", "composer.phar"]
 set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
 
 set :user, "www-data"
-set :keep_releases, 5
 set :update_vendors, true
 
 role :app, domain
